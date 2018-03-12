6cd9b6e966c4e3d9f801f5d6ba2e3bd080e5769a
Barchenko1 <wyerer@mail.ru>
2018-03-12 22:45:07 +0200

    factorial

---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/practices/FactorialNotRec.java
@@ -0,0 +1,13 @@
+package ua.org.oa.evlashdv.lectures.lecture1.practices;
+
+public class FactorialNotRec {
+    static int fact(int num){
+        int fact=1;
+        for (;num>0;fact*=num--);
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FactorialRec.fact(3));
+    }
+}
---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/practices/FactorialRec.java
@@ -0,0 +1,10 @@
+package ua.org.oa.evlashdv.lectures.lecture1.practices;
+
+public class FactorialRec {
+    static int fact(int num){
+        return (num==0) ? 1 : num*fact(num-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(FactorialRec.fact(3));
+    }
+}
