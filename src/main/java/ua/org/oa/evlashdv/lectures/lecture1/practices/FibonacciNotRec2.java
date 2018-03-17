632db771063ff1294224684287b47c3b88045899
Barchenko1 <wyerer@mail.ru>
2018-03-17 20:47:37 +0200

    addition

---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/practices/FibonacciNotRec2.java
@@ -0,0 +1,19 @@
+package ua.org.oa.evlashdv.lectures.lecture1.practices;
+
+public class FibonacciNotRec2 {
+    public static void main(String[] args) {
+        System.out.println(FibonacciNotRecursive(6));
+    }
+
+    private static int FibonacciNotRecursive(int n) {
+        if (n==0) return 1;
+        int f0=1;
+        int f1=1;
+        for (int i=2;i<n;++i){
+            int temp=f1;
+            f1+=f0;
+            f0=temp;
+        }
+        return f1;
+    }
+}
---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/practices/IsPrime.java
@@ -0,0 +1,17 @@
+package ua.org.oa.evlashdv.lectures.lecture1.practices;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        System.out.println(isPrime(7));
+    }
+
+    private static boolean isPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
