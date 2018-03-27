227895c6617ec8d7efa885f971b2894ac6f31458
Barchenko1 <wyerer@mail.ru>
2018-03-27 21:21:48 +0300

    Thread

---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/lecture7b/MyThread.java
@@ -0,0 +1,21 @@
+package ua.org.oa.evlashdv.lectures.lecture1.lecture7b;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        Thread t=new Thread(new MyThread());
+        System.out.println("Start Thread name- "+getName());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getName()+" -- "+i);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                return;
+                //e.printStackTrace();
+            }
+        }
+        System.out.println("End Thread name- "+getName());
+    }
+
+}
+
