61ef570143bfab7d5d69aeb6989a3097e14a35ea
Barchenko1 <wyerer@mail.ru>
2018-03-28 18:45:29 +0300

    Thread

---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/lecture7b/AutoRunThread.java
@@ -0,0 +1,21 @@
+package ua.org.oa.evlashdv.lectures.lecture1.lecture7b;
+
+public class AutoRunThread extends Thread{
+    public AutoRunThread() {
+        start();
+    }
+    @Override
+    public void run(){
+        System.out.println("Start Thread name= "+getName());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getName()+" -+ "+i);
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                return;
+                //e.printStackTrace();
+            }
+        }
+        System.out.println("End Thread name= "+getName());
+    }
+}
