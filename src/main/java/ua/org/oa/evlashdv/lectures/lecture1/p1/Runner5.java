7afc372cb25e27491e77c0b8ff168945f1a7ac7b
Barchenko1 <wyerer@mail.ru>
2018-05-10 01:21:23 +0300

    Oop

---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/p1/Runner5.java
@@ -0,0 +1,37 @@
+package ua.org.oa.evlashdv.lectures.lecture1.p1;
+
+import java.io.Console;
+import java.util.InputMismatchException;
+
+class Helper{
+    public void readFromConsole() {
+        Console con = System.console();
+        if (con != null) {
+            con.printf("Введите числовой код:");
+            int code = 0;
+            try {
+                code = Integer.valueOf(con.readLine());
+                System.out.println("Код доступа:" + code); } catch (InputMismatchException e) {
+                con.printf("неправильный формат кода" + e);
+            }
+            if (code != 0) {
+                con.printf("Введите пароль:");
+                String password;
+                char passTemp[] =
+                        con.readPassword("Введите пароль: ");
+                password = new String(passTemp);
+                System.out.println("Пароль:" + password);
+            }
+        } else {
+            System.out.println("Консоль недоступна");
+        }
+    }
+}
+
+public class Runner5 {
+    public static void main(String[] args) {
+        Helper helper=new Helper();
+        helper.readFromConsole();
+
+    }
+}
---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/p1/RunnerThread.java
@@ -0,0 +1,56 @@
+package ua.org.oa.evlashdv.lectures.lecture1.p1;
+class Blocked{
+    private int i=1000;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+    synchronized void doWait(){
+        System.out.print("Не ");
+        try {
+            this.wait();
+            System.out.print("сущностей ");
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int j = 0; j <5 ; j++) {
+            System.out.print("сверх ");
+        }
+    }
+}
+public class RunnerThread {
+    public static void main(String[] args) {
+        Blocked lock =new Blocked();
+        new Thread(){
+            @Override
+            public void run() {
+                lock.doWait();
+            }
+        }.start();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        synchronized (lock){
+            lock.setI(lock.getI()+3);
+            System.out.print("преумножай ");
+            lock.notify();
+        }
+        synchronized (lock){
+            lock.setI(lock.getI()+4);
+            System.out.print("необходимого. ");
+            try {
+                lock.wait(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.print("="+lock.getI());
+    }
+}
