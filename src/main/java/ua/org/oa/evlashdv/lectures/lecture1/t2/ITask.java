d304ef387c7bed3c579eb3f4e56f02a988c07572
Barchenko1 <33847867+Barchenko1@users.noreply.github.com>
2018-06-03 01:10:22 +0300

    Delete ITask.java

--- src/main/java/ua/org/oa/evlashdv/lectures/lecture1/t2/ITask.java
+++
@@ -1,6 +0,0 @@
-package ua.org.oa.evlashdv.lectures.lecture1.t2;
-public interface ITask {
-    public void loadFromFile() throws InvalidFileFormatException;
-    public void solution();
-    public void writeToFile();
-}
