480755b27379f84a89d266b8b1646622d0d9657d
Barchenko1 <wyerer@mail.ru>
2018-07-20 00:11:27 +0300

    hibernate

--- src/main/java/ua/org/oa/evlashdv/lectures/lecture1/hibernate/App1.java
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/hibernate/App1.java
@@ -5,17 +5,37 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
+
 public class App1 {
     public static void main(String[] args) {
-        UserDetails userDetails=new UserDetails();
-        userDetails.setUserId(1);
-        userDetails.setName("Pasha");
+        UserDetails user=new UserDetails();
+
+        user.setName("First User");
+        user.setAddress("First User's address");
+        user.setJoinedDate(new Date());
+        user.setDescription("description of the user goes here");
+
+        UserDetails user2=new UserDetails();
+
+        user2.setName("Second User");
+        user2.setAddress("Second User's address");
+        user2.setJoinedDate(new Date());
+        user2.setDescription("description of the user goes here");
 
         SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
         Session session=sessionFactory.openSession();
         session.beginTransaction();
-        session.save(userDetails);
+        session.save(user);
+        session.save(user2);
         session.getTransaction().commit();
         session.close();
+
+        user=null;
+
+        session=sessionFactory.openSession();
+        session.beginTransaction();
+        user=session.get(UserDetails.class,2);
+        System.out.println("user name retrieved is "+user.getName());
     }
 }
--- src/main/java/ua/org/oa/evlashdv/lectures/lecture1/hibernate/UserDetails.java
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/hibernate/UserDetails.java
@@ -1,14 +1,21 @@
 package ua.org.oa.evlashdv.lectures.lecture1.hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 
 @Entity
+@Table(name = "User_Details")
 public class UserDetails {
     @Id
+    @GeneratedValue
     private int userId;
-    private String Name;
+    private String userName;
+    private String Address;
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+    @Lob
+    private String Description;
 
     public int getUserId() {
         return userId;
@@ -19,16 +26,43 @@ public class UserDetails {
     }
 
     public String getName() {
-        return Name;
+        return userName;
     }
 
     public void setName(String name) {
-        Name = name;
+        userName = name;
     }
 
-    public UserDetails(int userId, String name) {
+    public Date getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(Date joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public UserDetails(int userId, String userName, String address, Date joinedDate, String description) {
         this.userId = userId;
-        Name = name;
+        this.userName = userName;
+        Address = address;
+        this.joinedDate = joinedDate;
+        Description = description;
     }
 
     public UserDetails() {
