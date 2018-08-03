ed0da92aeeb3e03234bc85ceadab944dec648acf
Barchenko1 <wyerer@mail.ru>
2018-08-04 00:12:55 +0300

    test

---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/testPractice/Demo.java
@@ -0,0 +1,14 @@
+package ua.org.oa.evlashdv.lectures.lecture1.testPractice;
+public class Demo{
+	public static void main(String[] args) {
+		Part1.main(new String[]{});
+		Part2.main(new String[]{"2","2"});
+		Part3.main(new String[]{"12423t34yh3wh3wh"});
+		Part4.main(new String[]{"4","6"});
+		Part5.main(new String[]{"123"});
+		Part6.main(new String[]{"10"});
+		Part7.main(new String[]{"27","AA","AD"});
+		
+	}
+
+}
