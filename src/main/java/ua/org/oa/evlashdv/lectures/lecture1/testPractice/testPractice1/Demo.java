cab338a0c9af2a52ed9378d06de9952da53161e0
Barchenko1 <wyerer@mail.ru>
2018-08-14 01:21:42 +0300

    p1

--- src/main/java/ua/org/oa/evlashdv/lectures/lecture1/testPractice/testPractice1/Demo.java
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/testPractice/testPractice1/Demo.java
@@ -1,13 +1,20 @@
 package ua.org.oa.evlashdv.lectures.lecture1.testPractice.testPractice1;
 public class Demo{
 	public static void main(String[] args) {
+		System.out.println("===Part1===");
 		Part1.main(new String[]{});
+		System.out.println("===Part2===");
 		Part2.main(new String[]{"2","2"});
+		System.out.println("===Part3===");
 		Part3.main(new String[]{"12423t34yh3wh3wh"});
+		System.out.println("===Part4===");
 		Part4.main(new String[]{"4","6"});
+		System.out.println("===Part5===");
 		Part5.main(new String[]{"123"});
+		System.out.println("===Part6===");
 		Part6.main(new String[]{"10"});
-		Part7.main(new String[]{"29","AC","AE"});
+		System.out.println("===Part7===");
+		Part7.main(new String[]{"A","B","Z","AA","AZ","BA","ZZ","AAA"});
 		
 	}
 
--- src/main/java/ua/org/oa/evlashdv/lectures/lecture1/testPractice/testPractice1/Part7.java
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/testPractice/testPractice1/Part7.java
@@ -2,14 +2,27 @@ package ua.org.oa.evlashdv.lectures.lecture1.testPractice.testPractice1;
 
 public class Part7 {
     public static void main(String[] args) {
-        int digit1 = Integer.parseInt(args[0]);
+
+        String char0 = String.valueOf(args[0]);
         String char1 = String.valueOf(args[1]);
         String char2 = String.valueOf(args[2]);
-        System.out.println(digit1 + " ==> " + Part7.digitsToChars(digit1));
-        System.out.println(char1 + " ==> " + Part7.charsToNumbers(char1));
-        System.out.println(char2 + " ==> " + Part7.rightColumn(char2));
+        String char3 = String.valueOf(args[3]);
+        String char4 = String.valueOf(args[4]);
+        String char5 = String.valueOf(args[5]);
+        String char6 = String.valueOf(args[6]);
+        String char7 = String.valueOf(args[7]);
+
+        System.out.println(char0+" ==> "+chars2Numbers(char0)+" ==> "+digits2Chars(chars2Numbers(char0)));
+        System.out.println(char1+" ==> "+chars2Numbers(char1)+" ==> "+digits2Chars(chars2Numbers(char1)));
+        System.out.println(char2+" ==> "+chars2Numbers(char2)+" ==> "+digits2Chars(chars2Numbers(char2)));
+        System.out.println(char3+" ==> "+chars2Numbers(char3)+" ==> "+digits2Chars(chars2Numbers(char3)));
+        System.out.println(char4+" ==> "+chars2Numbers(char4)+" ==> "+digits2Chars(chars2Numbers(char4)));
+        System.out.println(char5+" ==> "+chars2Numbers(char5)+" ==> "+digits2Chars(chars2Numbers(char5)));
+        System.out.println(char6+" ==> "+chars2Numbers(char6)+" ==> "+digits2Chars(chars2Numbers(char6)));
+        System.out.println(char7+" ==> "+chars2Numbers(char7)+" ==> "+digits2Chars(chars2Numbers(char7)));
+
     }
-    public static int charsToNumbers(String number) {
+    public static int chars2Numbers(String number) {
         int numeric = 0;
         for (int i = 1,j=number.length(); j > 0;i++, j--) {
             numeric += (int) (number.toUpperCase().charAt(number.length() - i) - 64)
@@ -18,7 +31,7 @@ public class Part7 {
         return numeric;
     }
 
-    public static String digitsToChars(int num) {
+    public static String digits2Chars(int num) {
         String chars = "";
         String charsCoup  = "";
         int module;
@@ -40,9 +53,9 @@ public class Part7 {
     public static String rightColumn(String num) {
         String chars = "";
         int num2;
-        num2 = charsToNumbers(num);
+        num2 = chars2Numbers(num);
         num2++;
-        chars = digitsToChars(num2);
+        chars = digits2Chars(num2);
         return chars;
     }
 
