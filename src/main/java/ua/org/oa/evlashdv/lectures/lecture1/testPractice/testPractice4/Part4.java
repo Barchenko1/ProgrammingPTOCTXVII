e2abb6d8411a6d5c818e84b62276dee71af33240
Barchenko1 <wyerer@mail.ru>
2018-09-14 00:12:42 +0300

    p4

---
+++ src/main/java/ua/org/oa/evlashdv/lectures/lecture1/testPractice/testPractice4/Part4.java
@@ -0,0 +1,17 @@
+package ua.org.oa.evlashdv.lectures.lecture1.testPractice.testPractice4;
+
+
+
+public class Part4 {
+	private static final String FILE_NAME = "part4.txt";
+
+	private static final String ENCODING = "Cp1251";
+
+
+	public static void main(String[] args) {
+		Parser parser = new Parser(FILE_NAME, ENCODING);
+		for (String str : parser) {
+			System.out.println(str);
+		}
+	}
+}
