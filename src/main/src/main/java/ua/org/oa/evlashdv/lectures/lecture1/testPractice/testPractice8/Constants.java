d98122597f9347a7975b8b872436d4425f960474
Barchenko1 <wyerer@mail.ru>
2018-10-05 01:22:43 +0300

    p8

---
+++ src/main/src/main/java/ua/org/oa/evlashdv/lectures/lecture1/testPractice/testPractice8/Constants.java
@@ -0,0 +1,23 @@
+package ua.org.oa.evlashdv.lectures.lecture1.testPractice.testPractice8;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Constants {
+    private static String URL="jdbc:mysql://localhost:3306/mydbtest";
+    private static String NAME="root";
+    private static String PASS="878988qqq";
+    public static Connection getConnection() {
+        Connection conn=null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(URL,NAME,PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
