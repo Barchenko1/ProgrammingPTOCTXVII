0079bd9732fd1d72d730830689451478e36720b2
Barchenko1 <wyerer@mail.ru>
2018-10-06 04:20:00 +0300

    p8

---
+++ src/main/src/main/java/ua/org/oa/evlashdv/lectures/lecture1/testPractice/testPractice8/User.java
@@ -0,0 +1,40 @@
+package ua.org.oa.evlashdv.lectures.lecture1.testPractice.testPractice8;
+
+public class User {
+    private int id;
+    private String login;
+
+    public User(String login) {
+        this.login = login;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public static User createUser(String name) {
+        return new User(name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                '}';
+    }
+}
