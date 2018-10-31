07fd383d7357fcb0aeb073ac942c899622e08901
Barchenko1 <wyerer@mail.ru>
2018-10-31 16:10:55 +0200

    service soap

---
+++ src/main/src/main/java/ua/org/oa/evlashdv/lectures/lecture1/servletsNew/Product.java
@@ -0,0 +1,51 @@
+package ua.org.oa.evlashdv.lectures.lecture1.servletsNew;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+
+@XmlRootElement(name="Product")
+@XmlType(propOrder= {"price","sku","name"})
+public class Product implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+    private String sku;
+    private double price;
+
+    public Product(String name, String sku, double price) {
+        this.name = name;
+        this.sku = sku;
+        this.price = price;
+    }
+    public Product() {
+    	
+    }
+    @XmlElement(name="ProductName")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
